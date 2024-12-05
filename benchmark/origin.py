93a64c20260e52cd0c2c8b2879ce1ae868ed2227
brian <brian030128@gmail.com>
2024-12-05 18:04:10 +0800

    update

--- benchmark/origin.py
+++ benchmark/origin.py
@@ -14,11 +14,12 @@ def origin_generate(model, tokenizer, prompt, num_beams, max_new_tokens) -> Tupl
 
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
 
+    print("shape", input_ids.shape)
     attention_mask = torch.ones_like(input_ids)
     outputs = model.generate(input_ids,attention_mask=attention_mask, do_sample=False, num_beams=num_beams, max_new_tokens=max_new_tokens, temperature=None, top_p = None, early_stopping=True)
 
     # Decode and print the generated output
-    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    generated_text = tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
     return (generated_text, LlamaForCausalLM.used_gpu, LlamaForCausalLM.time_metric)
 
 
