848777a42d4b645e0be67ab60e6f4d86c8324baf
brian <brian030128@gmail.com>
2024-12-05 20:58:00 +0800

    update

--- benchmark/parse_human_eval.py
+++ benchmark/parse_human_eval.py
@@ -4,6 +4,10 @@ import json
 in_folder = "out/tree"
 out_folder = "out/human_eval"
 
+all_ids = set()
+for i in range(164):
+    all_ids.add(f"HumanEval/{i}")
+
 for filename in os.listdir(in_folder):
     file_path = os.path.join(in_folder, filename)
         
@@ -12,11 +16,17 @@ for filename in os.listdir(in_folder):
         continue
     with open(file_path, 'r', encoding='utf-8') as file:
         with open(os.path.join(out_folder, filename), 'w') as out_file:
+            all_id_copy = all_ids.copy()
             for line in file:
                 data = json.loads(line)
-                print(data.keys())
                 out_file.write(json.dumps({
                     "task_id": data['id'],
                     "completion": data['output']
+                }) + "\n")
+                all_id_copy.remove(data['id'])
+            for id in all_id_copy:
+                out_file.write(json.dumps({
+                    "task_id": id,
+                    "completion": ""
                 }) + "\n")
         
