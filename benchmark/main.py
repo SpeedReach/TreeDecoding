aba32258646e6dafbfbea219acb68cef8e6e9c76
Sleep_AllDay <37238439+SpeedReach@users.noreply.github.com>
2025-01-16 10:45:12 +0800

    Update main.py

--- benchmark/main.py
+++ benchmark/main.py
@@ -23,7 +23,7 @@ sys.setrecursionlimit(3000)
 
 
 
-model_name = "meta-llama/Llama-2-7b-hf"
+model_name = "meta-llama/Llama-2-7b-chat-hf" 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
